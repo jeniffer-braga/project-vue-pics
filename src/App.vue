f0b203040f4aedb32d2ce7f6a95c029ae4d7aefc
jeniffer-braga <jeniffercunhabraga@gmail.com>
2022-02-01 11:27:14 -0300

    Exibindo imagens consumindo dados de uma API

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,29 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div>
+		<h1>{{ title }}</h1>
+		<ul>
+			<li v-for="picture of pictures" :key="picture">
+				<img :src="picture.url" :alt="picture.title">
+			</li>
+		</ul>
+	</div> 
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'TESTE'
-    }
-  }
+	export default {
+		data() {
+			return {
+				title: 'Vue pictures',
+				pictures: []
+			}
+		},
+
+		created() {
+			this.$http.get('http://localhost:3000/v1/fotos')
+				.then(res => res.json())
+				.then(resPictures => this.pictures = resPictures, err => console.log(err)); 
+		}
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
+<style></style>
