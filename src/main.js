321bc5bdbcd6ba03ce9b5d6563e3b22505b31bfd
jeniffer-braga <jeniffercunhabraga@gmail.com>
2022-02-11 16:09:11 -0300

    Importação de arquivos css

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import { routes } from './routes'; 
 
+import 'bootstrap/dist/css/bootstrap.css'; 
+import './assets/css/teste.css'; 
+
 Vue.use(VueResource); 
 Vue.http.options.root = 'http://localhost:3000';
 Vue.use(VueRouter); 
--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,22 @@ module.exports = {
         options: {
           name: '[name].[ext]?[hash]'
         }
+      },
+      { 
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      { 
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream' 
+      },
+      { 
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'file-loader' 
+      },
+      { 
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml' 
       }
     ]
   },
